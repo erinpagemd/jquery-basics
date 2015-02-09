25129b0b40d7ee5e62f81adb0b5cdd9c1e8ebb8f
YOUR NAME <erinpagemd@yahoo.com>
2015-02-08 23:47:57 -0600

    practicing functional programming by breaking up the functions into smaller modules

--- main.js
+++ main.js
@@ -10,38 +10,37 @@ function initialize(){
 function addText(event){
   event.preventDefault();
   $('#a').text(getValue(event));
-  $('#word').val(" ");
 }
 
 //use entered text to change css color property of "b" div
 function changeColor(event){
   event.preventDefault();
   $('#b').css('background-color', getValue(event));
-  $('#rawColor').val(" ");
 }
 
 //convert 2 entered number separated by a + into a sum and append to "c" div
 function addSum(event){
   event.preventDefault();
-  //get the value of the input with id numbers
-  var numbers = getValue(event);
-  //turn numbers into an array
-  numbers = numbers.split('+').map(Number);
-
-  var x = numbers[0];
-  var y = numbers[1];
-
-  var answer = x + y;
-  $('#c').text(answer);
-
-  $('#numbers').val(" ");
+  $('#c').text(addNumbers(getValue(event)));
 }
 
+//take a string, turn it into an array of 2 numbers, add the 2 numbers together
+function addNumbers(string){
+  var numbers = string.split('+').map(Number);
+  var x = numbers[0];
+  var y = numbers[1];
+  var answer = x + y;
+  return answer;
+}
+
+//gets the value of the input field that is the only input sibling of the event target
 function getValue(event){
   //get the id of the input that is the sibling of the event target
-  var inputID = $(event.target).siblings('input').attr('id');
+  var $inputID = $(event.target).siblings('input').attr('id');
   //use the id to get the value of input field
-  var $inputContent = $('#' + inputID).val();
+  var $inputContent = $('#' + $inputID).val();
+  //clear the input field
+  $('#' + $inputID).val(" ");
   //return the id of the input field
-  return ($inputContent);
+  return $inputContent;
 }
